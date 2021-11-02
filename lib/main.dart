877f4f9c4356da0cefd0f6fb81c61929647b575a
松坂波季 <matuharu@matsuzakahakinoMacBook-Pro.local>
2021-11-02 12:10:56 +0900

    ドキュメントを作成

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyFirestorePage(),
+    );
+  }
+}
+
+class MyFirestorePage extends StatefulWidget {
+  MyFirestorePage({Key? key}) : super(key: key);
+
+  @override
+  _MyFirestorePageState createState() => _MyFirestorePageState();
+}
+
+class _MyFirestorePageState extends State<MyFirestorePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              child: const Text("コレクション＋ドキュメント追加"),
+              onPressed: () async {
+                await FirebaseFirestore.instance
+                    .collection("users")
+                    .doc("id_abc")
+                    .set({"name":"松坂","age":40});
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
